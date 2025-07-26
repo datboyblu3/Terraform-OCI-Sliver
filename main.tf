5341d8d06f08a0213265b3405cf35b4de99438df
datboyblu3 <datboyblu3@protonmail.com>
2025-07-26 12:15:02 -0400

    moving stuff around

--- main.tf
+++ main.tf
@@ -1,3 +1,54 @@
+# Create the KMS Vault
+resource "oci_kms_vault" "sato_kms_vault" {
+    compartment_id = var.compartment # Use a variable for compartment_id
+      display_name   = "sato-vault"
+      vault_type     = "DEFAULT"
+    }
+
+# Create the master symmetric key
+resource "oci_kms_key" "sato_secret_key" {
+  management_endpoint = oci_kms_vault.sato_kms_vault.management_endpoint
+  key_shape {
+    algorithm = "AES"
+    length    = 256    
+  }
+  display_name = "sato-secret-encryption-key"
+  compartment_id = var.compartment_ocid
+}
+
+# Define the secret 
+resource "oci_vault_secret" "sato_secret" {
+  compartment_id = var.compartment_ocid
+  vault_id       = oci_kms_vault.my_kms_vault.id
+  key_id         = oci_kms_key.my_secret_key.id
+  secret_name    = "oci_compute_secret"
+  enable_auto_generation = var.secret_enable_auto_generation
+
+secret_generation_context {
+  generation_type     = "PASSWORD"
+  generation_template = "OCI_GENERATED_PASSWORD"
+  passphrase_length   = 20  
+ }
+
+secret_rules {
+  rule_type = "SECRET_REUSE_RULE"
+  is_enforced_on_deleted_secret_versions = true
+ }
+
+secret_rules {
+  rule_type = "SECRET_EXPIRY_RULE"
+  secret_version_expiry_interval = "P5D" # Expire in 5 days
+ }
+
+}
+
+
+# Get secret from OCI Vault
+data "oci_secrets_secretbundle" "password_from_vault"{
+  secret_id = oci_vault_secret.sato_secret.id
+}
+
+
 locals {
     opc_new_password = base64decode(data.oci_secrets_secretbundle.password_from_vault.oci_secrets_secretbundle{0}.content)
 }
@@ -128,12 +179,9 @@ resource "oci_core_security_list" "security_lab_fw" {
     description = "Allow outbound TCP traffic"
   }
   
-
 }
 
  
-
-
 resource "oci_core_instance" "security_lab_instance" {
   availability_domain = data.oci_identity_availability_domains.security_lab_ad.availability_domains[0].name
   compartment_id      = var.compartment
@@ -167,11 +215,17 @@ resource "oci_core_instance" "security_lab_instance" {
     }
   }
 
- 
-
   provisioner "local-exec" {
     working_dir = var.dir
     command     = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${self.public_ip},'  -u opc --private-key '~/.ssh/oci_key' -e '~/.ssh/oci_key.pub' get_stager.yml -vvv --ask-become-pass"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      # Change opc password
+      "echo 'opc:${local.opc_new_password}' | sudo chpasswd",
+      "echo 'opc password updated successfully!"
+    ]
   }
 
 }
